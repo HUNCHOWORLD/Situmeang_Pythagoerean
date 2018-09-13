c3eec092b3df5c4e8a29da54d57564279acbb4d0
Kalvin <kalvinsit.81381@redlandsschools.net>
2018-09-13 14:58:30 +0000

    hi

---
+++ Situmeang_pythagorean.py
@@ -0,0 +1,8 @@
+"""
+write a program that will ask the user to enter their name 
+and respond with a greeting using there name
+"""
+
+print (" Hello! Welcome! ")
+name = input("what is your name? ")
+print (" hello " +name+ "")
