89aad6e4792aeaec122e9d2936978899935075c4
Kalvin <kalvinsit.81381@redlandsschools.net>
2018-09-15 01:32:00 +0000

    hi

--- Situmeang_pythagorean.py
+++ Situmeang_pythagorean.py
@@ -2,7 +2,7 @@
 write a program that will ask the user to enter their name 
 and respond with a greeting using there name
 """
-
+from math import sqrt
 print (" Hello! Welcome! ")
 
        
@@ -10,4 +10,14 @@ name = raw_input ("what is your name? ")
  
 print ("Hello " + name + "!")
 
+#lengths for pythagorean T
 
+a = float(input("Please input side a: "))
+
+b = float(input("Please inpute side b: "))
+
+c = sqrt( a**2 + b**2)
+
+hyp = sqrt (c)
+
+print("The hypotenuse of a right angled triangle with side a =",a,"and side b =",b,"equals ",hyp )
