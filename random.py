15d2a86d8d0e19152ead8ef61de98e5de992131c
Kalvin <kalvinsit.81381@redlandsschools.net>
2018-09-17 15:17:52 +0000

    hi

---
+++ random.py
@@ -0,0 +1,27 @@
+"""
+This program demonstrates IMPORT statements and loops
+"""
+
+# import statements go first! (top of your code)
+import time
+
+#for variableName in range(start, stop,step):
+# Or
+# for variableName in range(stop): * This will start at 0 and step by 1
+
+for i in range (1,11):
+    print(' hello ' + str(i))
+
+# count to 100 by 5's
+
+for i in range (5,103,5):
+    print(' Hello ' +  str(i))
+
+# count down from (start at 20) by 2's and stop at -4
+
+for i in range (20,-5,-2):
+    print ('hello ' + str(i))
+    
+
+
+    
