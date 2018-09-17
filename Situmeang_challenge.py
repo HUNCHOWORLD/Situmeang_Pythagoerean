4732e5ea0b5f2023fd9df87e34e86f3e73b29d2e
Kalvin <kalvinsit.81381@redlandsschools.net>
2018-09-17 08:17:05 +0000

    hi

---
+++ Situmeang_challenge.py
@@ -0,0 +1,17 @@
+from math import sqrt
+print (" Hello! Welcome! ")
+
+       
+name = raw_input ("what is your name? ")
+ 
+print ("Hello " + name + "!")
+
+#lengths for pythagorean T
+
+a = float(input("Please input side a: "))
+
+b = float(input("Please input side b: "))
+
+c = float(input("Please input side c: "))
+
+c = sqrt( a**2 + b**2 == c)
